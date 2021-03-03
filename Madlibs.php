6a4a1cfc3f9f88385b4b2bc595ff9a1c29a1f561
Thomas <99062905@mydavinci.nl>
2021-03-03 11:13:10 +0100

    ja

---
+++ Madlibs.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link type="text/css" rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div class= "container">
+		<h1>MadLibs</h1>
+
+		<div class= "redbar">
+			<p>Er heerst paniek.....</p>
+			<p>Onkunde</p>
+		</div>
+
+		<div class= "whitefield">
+			<label for= "v1" class= "l1">Wat zou je graag willen kunnen</label>
+			<input type="text" name="v1" class= "v1">
+
+			<label for= "v2" class= "l2">Met welke persoon kun je goed opschieten</label>
+			<input type="text" name="v3" class= "v2">
+
+			<label for= "v3" class= "l3">asd</label>
+			<input type="text" name="v3" class= "v3">
+		</div>
+	</div>
+</body>
+</html>
